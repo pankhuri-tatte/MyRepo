90794f39e4c676188c7c78c9b40d40b2a5445baa
pankhuri-tatte <47206369+pankhuri-tatte@users.noreply.github.com>
2019-11-25 11:37:40 +0530

    Update Electricy_Bill.java

--- Electricy_Bill.java
+++ Electricy_Bill.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
 
-class Ele_Bill {
+class Electricity_Bill {
+	
 	public static void main(String args[]) {
 		long units;
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("enter number of units");
+		System.out.println("Enter number of units");
 
 		units = sc.nextLong();
 
 		double billpay = 0;
 
 		if (units < 100)
-			billpay = units * 1.20;
+			billpay = units * 5;
 
-		else if (units < 300)
-			billpay = 100 * 1.20 + (units - 100) * 2;
+		else if (units <= 300)
+			billpay = 100 * 5 + (units - 100) * 10;
 
 		else if (units > 300)
-			billpay = 100 * 1.20 + 200 * 2 + (units - 300) * 3;
+			billpay = 100 * 5 + 200 * 10 + (units - 300) * 20;
 
-		System.out.println("Bill to pay : " + billpay);
+		System.out.println("Electricity consumption : " + billpay);
 	}
 }
